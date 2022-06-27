90899116a499a8d06b21c867412452616540c49f
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-27 13:17:36 +0200

    docu finished

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3334;
+const port = 3334;
 
 app.get('/', (req, res) => {
-	res.send('<h1>Employee API</h1>');
+    res.send(`
+        <style>
+        li {
+            font-size: 1.5rem;
+        }
+        </style>
+        <h1>Employee API</h1>
+        <ul>
+            <li><a href="http://localhost:${port}/employees">http://localhost:${port}/employees</a> = all employees</li>
+            <li><a href="http://localhost:${port}/employees/4">http://localhost:${port}/employees/4</a> = employee with ID 4</li>
+            <li><a href="http://localhost:${port}/employees-territories/4">http://localhost:${port}/employees-territories/4</a> = territories of employee with ID 4</li>
+        </ul>
+    `);
 });
 
-app.listen(PORT, () => {
-	console.log(`listening on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
 });
